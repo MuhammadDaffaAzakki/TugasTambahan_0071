c7ab83554658cc1e40bb56b3b86dd877332db9e5
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-04-30 22:59:18 +0700

    Membuat public 2 dan output data

--- tugastambahan.cpp
+++ tugastambahan.cpp
@@ -16,3 +16,14 @@ class Segitiga {
         float luas(int alas, int tinggi) {
             return 0.5 * alas * tinggi;
         }
+
+    public:
+        void outputData() {
+            if (alas % 2 == 0) {
+            float hasil = luas(alas, tinggi);
+            cout << "Luas Segitiga" << hasil << "cm";
+        } else {
+            cout << "Alas yang dimasukkan adalah bilangan ganjil, luas tidak dapat dihitung";
+        }
+    }
+};
