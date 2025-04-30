c8b52d24da987569a38251e2c11dd95d4dc7cdc8
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-04-30 22:42:27 +0700

    Membuat public dan input data

--- tugastambahan.cpp
+++ tugastambahan.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 class Segitiga {
 
+    public:
+        int alas, tinggi;
+        void inputData() {
+            cout << "Masukkan Alas Segitiga";
+            cin >> alas;
+            cout << "Masukkan Tinggi Segitiga";
+            cin >> tinggi;
+        }
